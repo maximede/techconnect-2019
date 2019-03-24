fda13d49b7ccc7d7ecd8f91bc5cba3284dda898b
Maxime Deravet <maximed@uw.edu>
2019-03-24 09:47:35 -0700

    Initial commit

---
+++ src/main/java/com/example/techconnect/TechconnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.techconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechconnectApplication.class, args);
+	}
+
+}
